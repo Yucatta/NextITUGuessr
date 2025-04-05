5d35554d17f973766fb9d3b0ae2caaf4d67e70e2
Yucatta <ahmetabali41@gmail.com>
2025-04-05 13:03:15 +0300

    changedconfig

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,5 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-module.exports = {
-  experimental: {
-    turbo: false, // Disable Turbopack
-  },
-};
 
 export default nextConfig;
